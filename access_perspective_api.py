29374445a7594810503881140c3fd2e11a019a5d
Braunstsf <113804405+Braunstsf@users.noreply.github.com>
2022-10-18 01:46:19 -0500

    Add files via upload

--- access_perspective_api.py
+++ access_perspective_api.py
@@ -1,20 +1,23 @@
 from googleapiclient import discovery
 import json
 
-API_KEY = 'AIzaSyDm9OBLzDnIvfxI4cH_dqSWwxwrD9I7GMg'
+API_KEY = XXX
 
-client = discovery.build(
-  "commentanalyzer",
-  "v1alpha1",
-  developerKey=API_KEY,
-  discoveryServiceUrl="https://commentanalyzer.googleapis.com/$discovery/rest?version=v1alpha1",
-  static_discovery=False,
-)
+def find_toxicity_score(comment): 
+    client = discovery.build(
+      "commentanalyzer",
+      "v1alpha1",
+      developerKey=API_KEY,
+      discoveryServiceUrl="https://commentanalyzer.googleapis.com/$discovery/rest?version=v1alpha1",
+      static_discovery=False,
+    )
 
-analyze_request = {
-  'comment': { 'text': 'friendly greetings from python' },
-  'requestedAttributes': {'TOXICITY': {}}
-}
+    analyze_request = {
+      'comment': { 'text': comment},
+      'requestedAttributes': {'TOXICITY': {}}
+    }
 
-response = client.comments().analyze(body=analyze_request).execute()
-print(json.dumps(response, indent=2))
+    response = client.comments().analyze(body=analyze_request).execute()
+    toxicity_score = response["attributeScores"]["TOXICITY"]["summaryScore"]["value"]
+    
+    return(toxicity_score)
